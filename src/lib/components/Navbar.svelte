a65c2dbe352fffb4e31e2fc2920d13ba042a3b0c
Din-Hot <dinhot02@gmail.com>
2024-06-07 18:39:01 +0200

    feat: "Simple navigation and layout of page"

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+	import { scale, slide } from 'svelte/transition';
+
+	let dropDownOpened = false;
+</script>
+
+<nav class="menu-bar">
+	<h1 class="logo"><a href="/">Din-Hot Advent of Svelte challenge</a></h1>
+	<ul>
+		<li><a href="About">About</a></li>
+		<li
+			on:mouseenter={() => (dropDownOpened = !dropDownOpened)}
+			on:mouseleave={() => (dropDownOpened = !dropDownOpened)}
+		>
+			Year <i class="fas fa-caret-down"></i>
+			{#if dropDownOpened}
+				<div transition:slide={{ duration: 300 }} class="dropdown-menu">
+					<ul transition:scale={{ duration: 370 }}>
+						<li><a href="/2023">2023</a></li>
+						<li><a href="/2024">2024 <br />(Not yet out)</a></li>
+					</ul>
+				</div>
+			{/if}
+		</li>
+		<li><a href="/contact">Contact me</a></li>
+	</ul>
+</nav>
+
+<style>
+	.logo a {
+		font-size: 1.25rem;
+		color: white;
+		text-decoration: none;
+	}
+
+	.logo a:hover {
+		color: rgba(255, 255, 255, 0.75);
+	}
+	.menu-bar {
+		height: 1.75rem;
+		background-color: #ff3e00;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0.5rem 4%;
+	}
+	.menu-bar ul {
+		list-style: none;
+		display: flex;
+	}
+	.menu-bar ul li {
+		padding: 0.5rem 2rem;
+		position: relative;
+	}
+	.menu-bar ul li a,
+	li {
+		font-size: 1rem;
+		color: white;
+		text-decoration: none;
+		cursor: pointer;
+		transition: all 0.3s;
+	}
+	.menu-bar ul li a:hover,
+	li:hover {
+		color: rgba(255, 255, 255, 0.75);
+	}
+	.fas {
+		float: right;
+		margin-left: 0.75rem;
+	}
+	/* dropdown menu style */
+	.dropdown-menu {
+		z-index: 1;
+		display: block;
+		position: absolute;
+		left: 0;
+		top: 100%;
+		background-color: #ff3e00;
+	}
+	.dropdown-menu ul {
+		display: block;
+		margin: 0.5rem;
+	}
+	.dropdown-menu ul li {
+		text-align: center;
+		width: 6rem;
+		padding: 0.5rem;
+	}
+</style>
---
+++ src/lib/components/snowflakes.svelte
@@ -0,0 +1,189 @@
+<script lang="ts">
+</script>
+
+<div class="containter">
+	<div class="title">
+		<h1>Advent of Svelte challenge by Din Hot</h1>
+		<p>My solutions to <a href="https://advent.sveltesociety.dev/">advent.sveltesociety.dev</a></p>
+	</div>
+	<div class="snowfall">
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+		<div class="snowflake"></div>
+	</div>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+	.containter {
+		position: relative;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.title {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+	}
+
+	h1 {
+		font-family: 'Poppins', sans-serif;
+		font-weight: 400;
+		font-style: normal;
+		color: white;
+		font-size: 5rem;
+		letter-spacing: 4px;
+		text-align: center;
+	}
+
+	p {
+		margin-top: 2rem;
+		color: white;
+		font-size: 1rem;
+		letter-spacing: 4px;
+		text-align: center;
+		font-family: 'Poppins', sans-serif;
+		font-weight: 100;
+		font-style: normal;
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+
+	.snowfall {
+		position: absolute;
+		z-index: -1;
+		inset: 0;
+		overflow: hidden;
+		background: linear-gradient(#22304e, #111827 85%);
+	}
+
+	.snowflake {
+		--small: 3px;
+		--medium: 7px;
+		width: var(--small);
+		height: var(--small);
+		position: absolute;
+		background-color: #fff;
+		border-radius: 50%;
+		animation:
+			snow 6s linear infinite,
+			wind 10s ease-in-out infinite;
+		top: -10%;
+		left: 10%;
+		opacity: 75%;
+	}
+
+	.snowflake:nth-child(2n + 3) {
+		/* 3, 5, 7, ... */
+		width: var(--medium);
+		height: var(--medium);
+	}
+
+	.snowflake:nth-child(1) {
+		left: 25%;
+		animation-delay: 1s;
+	}
+
+	.snowflake:nth-child(2) {
+		left: 85%;
+		animation-delay: 1.5s;
+	}
+
+	.snowflake:nth-child(3) {
+		left: 30%;
+		animation-delay: 2s;
+	}
+
+	.snowflake:nth-child(4) {
+		left: 65%;
+		animation-delay: 2.5s;
+	}
+
+	.snowflake:nth-child(5) {
+		left: 25%;
+		animation-delay: 3s;
+	}
+
+	.snowflake:nth-child(6) {
+		left: 95%;
+		animation-delay: 3.5s;
+	}
+
+	.snowflake:nth-child(7) {
+		left: 55%;
+		animation-delay: 4s;
+	}
+
+	.snowflake:nth-child(8) {
+		left: 10%;
+		animation-delay: 4.5s;
+	}
+
+	.snowflake:nth-child(9) {
+		left: 70%;
+		animation-delay: 5s;
+	}
+
+	.snowflake:nth-child(10) {
+		left: 20%;
+		animation-delay: 5.5s;
+	}
+	.snowflake:nth-child(11) {
+		left: 75%;
+		animation-delay: 6s;
+	}
+	.snowflake:nth-child(12) {
+		left: 15%;
+		animation-delay: 7s;
+	}
+	.snowflake:nth-child(13) {
+		left: 95%;
+		animation-delay: 7.5s;
+	}
+	.snowflake:nth-child(14) {
+		left: 5%;
+		animation-delay: 8s;
+	}
+	.snowflake:nth-child(15) {
+		left: 55%;
+		animation-delay: 8.5s;
+	}
+	.snowflake:nth-child(16) {
+		left: 30%;
+		animation-delay: 9s;
+	}
+
+	@keyframes snow {
+		100% {
+			top: 100%;
+		}
+	}
+
+	@keyframes wind {
+		50% {
+			transform: translateX(120px);
+		}
+	}
+</style>
